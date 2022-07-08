7c37d057b1beef230e98ba4458044f444dd16c88
Eduard Klimenko <edik.klimenko@gmail.com>
2022-07-08 12:45:20 +0300

    draft: Multi-database selector #8

--- src/app/components/login-form/login-form.component.ts
+++ src/app/components/login-form/login-form.component.ts
@@ -4,18 +4,36 @@ import { Component, OnInit, Input, Output, EventEmitter, ChangeDetectionStrategy
     selector: 'app-login-form',
     templateUrl: './login-form.component.html',
     styleUrls: ['./login-form.component.scss'],
-    changeDetection: ChangeDetectionStrategy.OnPush
+    // changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class LoginFormComponent {
+export class LoginFormComponent implements OnInit{
     @Input() isAccess: boolean = false;
     @Input() settings = {
         dbPassword: '',
         dbLogin: '',
         dbLink: ''
     };
+
+    dbItems: any[] = [
+    ];
+
     @Input() errorMessage: any = '';
+
     @Output() ready: EventEmitter<any> = new EventEmitter();
 
+    ngOnInit() {
+        this.dbItems = [
+            {
+                value: `${this.settings.dbLink}|${this.settings.dbLogin}`,
+                viewValue: `${this.settings.dbLink} | ${this.settings.dbLogin} | ${this.hidePassword(this.settings.dbPassword)}`
+            },
+            { value: 'pizza-1', viewValue: 'Pizza' },
+            { value: 'tacos-2', viewValue: 'Tacos' },
+        ];
+    }
+    hidePassword(str: string) {
+        return Array.from({ length: (str + '').length }, () => '•').join('');
+    }
     connectToDB() {
         this.ready.emit(this.settings);
     }
--- src/app/components/login-form/login-form.module.ts
+++ src/app/components/login-form/login-form.module.ts
@@ -5,7 +5,9 @@ import { LoginFormComponent } from './login-form.component';
 import { FormsModule } from '@angular/forms';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
-
+import { MatSelectModule } from '@angular/material/select';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatListModule } from '@angular/material/list';
 @NgModule({
     imports: [
         CommonModule,
@@ -13,6 +15,9 @@ import { MatButtonModule } from '@angular/material/button';
         MatInputModule,
         MatCardModule,
         MatButtonModule,
+        MatSelectModule,
+        MatExpansionModule,
+        MatListModule
     ],
     declarations: [LoginFormComponent],
     exports: [LoginFormComponent]
